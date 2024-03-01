2a03a99cb1f4683a30140274444d3df896528665
Mangesh3555 <161795581+Mangesh3555@users.noreply.github.com>
2024-03-01 10:03:00 +0530

    Update mangesh2.py

--- mangesh2.py
+++ mangesh2.py
@@ -1,1 +1,6 @@
-print('Hello, world!')
+arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+size = len(arr)
+
+print("The integral elements present in the array are: ")
+for i in range(0, size):
+   print(arr[i])
